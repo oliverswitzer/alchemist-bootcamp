d745573b2507aaee8c690b24b255ef60d52afa69
Oliver Switzer <oliverswitzer@gmail.com>
2020-07-21 12:52:30 -0400

    Add zip and zipper functions (tail recursive and non-tail recursive)

--- learn_you_some_erlang/recursion.ex
+++ learn_you_some_erlang/recursion.ex
@@ -15,4 +15,13 @@ defmodule Recursion do
   # Until the length of our accumulator reaches n, lets prepend the head of the list to the acc
   def sublist([head | tail], n, acc) when length(acc) < n, do: sublist(tail, n, [head | acc])
   def sublist([_ | tail], n, acc), do: sublist(tail, n, acc)
+
+  #  1> recursive:zip([a,b,c],[1,2,3])
+  # [{a,1},{b,2},{c,3}]
+  def zip(list1, list2), do: zip(list1, list2, []) |> Enum.reverse
+  def zip(list1, list2, acc) when length(list1) === 0 or length(list2) === 0, do: acc
+  def zip([h1|t1], [h2|t2], acc), do: zip(t1, t2, [{h1, h2} | acc])
+
+  def zipper([], []), do: []
+  def zipper([h1 | t1], [h2 | t2]), do: [{h1, h2} | zip(t1, t2)]
 end
